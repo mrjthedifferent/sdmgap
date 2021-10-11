a8b26262e7a4a97374ad742a4a7344f82b4d3a1d
Mahfuz <jolontojewel@gmail.com>
2021-10-11 21:13:59 +0600

    Added Images (Day4)

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,52 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(AntoraApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
+class AntoraApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'My First App',
-      theme: ThemeData(
-        primarySwatch: Colors.red,
-      ),
-      home: const MyHomePage(),
+      title: "Antora App",
+      home: MyHome(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
+class MyHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("My First App "),
+        title: Text("Antora App"),
       ),
-      body: const Center(
-        child: Text("Hello World "),
+      body: Center(
+        child: Container(
+          height: 200,
+          color: Colors.red,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                Image.network(
+                  "https://scontent.fdac14-1.fna.fbcdn.net/v/t39.30808-1/p100x100/231269042_368843997962364_2221301075560312016_n.jpg?_nc_cat=108&ccb=1-5&_nc_sid=7206a8&_nc_eui2=AeGTvb3Bz4fhPyFG7kW2R7dnVtjkY3AFXg9W2ORjcAVeD3Ox-blfRq4nx7AHL25ZKqAzqglUvUEkbDAnwlrmF1sj&_nc_ohc=9s6NbYQUmokAX-oqmF7&_nc_ht=scontent.fdac14-1.fna&oh=c897e2e4ab9e41124ec468bde0d5a5a8&oe=616845F0",
+                ),
+                Text(
+                  "Antora Kundu",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  "Ab20 Batch",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
